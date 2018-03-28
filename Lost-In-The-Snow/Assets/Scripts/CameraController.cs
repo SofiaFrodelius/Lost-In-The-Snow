0d7f092537a94b95d580bc6788a68c1f52d8e27d
HolmErik <erikholm@live.com>
2018-03-28 14:26:22 +0200

    Camera Controller
    Basic camera controller

--- Lost-In-The-Snow/Assets/Scripts/CameraController.cs
+++ Lost-In-The-Snow/Assets/Scripts/CameraController.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 1.0f;
+    private Vector2 look = Vector2.zero;
 
-	// Use this for initialization
-	void Start () {
-        //Cursor.lockState = CursorLockMode.Locked;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        move *= sensitivity;
+        look += move;
+        look.y = Mathf.Clamp(look.y, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(-look.y, Vector3.right);
+        transform.parent.transform.localRotation = Quaternion.AngleAxis(look.x, transform.parent.transform.up);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
     }
 }
