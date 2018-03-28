ac4c8817312c79a0672441a9f4053609d97679ab
Sooolan <emil.rosenquist@gmail.com>
2018-03-28 11:33:42 +0200

    CharacterController & CameraController & Materials

---
+++ Lost-In-The-Snow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    CharacterController characterController;
+    public float speed = 10f;
+    private void Awake(){
+        characterController = GetComponent<CharacterController>();
+    }
+    void Update(){
+        Vector3 movingVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+            movingVelocity.y = 209f;
+        else
+            movingVelocity.y -= 9.82f*Time.deltaTime;
+        characterController.Move(speed * movingVelocity * Time.deltaTime);
+    }
+}
