07129c09f4079e126455ffcdd938ed66cf3e1a87
SofiaFrodelius <scris_love@hotmail.com>
2018-03-28 15:17:50 +0200

    Menu

---
+++ Lost-In-The-Snow/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void StartBtn(string SceneName)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OptionsBtn(string SceneName)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void BackBtn(string SceneName)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void ExitBtn()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
