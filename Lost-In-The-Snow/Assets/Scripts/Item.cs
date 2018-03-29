12beb5859970b9a7e4db81e824476efe948556f2
HolmErik <erikholm@live.com>
2018-03-29 10:11:09 +0200

    fix
    fix

---
+++ Lost-In-The-Snow/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private int id;
+    private string name;
+    private int itemAmmount;
+
+    public Item(int tID, string tName)
+    {
+        id = tID;
+        name = tName;
+    }
+}
