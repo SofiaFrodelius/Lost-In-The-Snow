fee356cda29af9153497c1f90dce2c417451b108
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-04-06 10:17:39 +0200

    fix
    fix tree support

---
+++ Lost-In-The-Snow/Assets/JohansDoNotTouch/Scripts/InGamePause.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class InGamePause {
+    private static bool isPaused = false;
+	
+
+	public static void TogglePause () {
+        isPaused = !isPaused;
+        
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            return;
+        }
+        Time.timeScale = 0;
+	}
+	
+}
