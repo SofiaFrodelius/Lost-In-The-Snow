979abf72c8e729a21961beae8c1cb5810f5e099e
Sooolan <emil.rosenquist@gmail.com>
2018-04-06 15:15:20 +0200

    LeadPlayer Dog

--- Lost-In-The-Snow/Assets/Scripts/Dog/Actions/LeadPlayer.cs
+++ Lost-In-The-Snow/Assets/Scripts/Dog/Actions/LeadPlayer.cs
@@ -6,9 +6,37 @@ using UnityEngine.AI;
 public class LeadPlayer : DogAction{
     Transform player;
     Transform target;
-    public LeadPlayer(Dog d, Transform player, Transform target) : base(d){
+	float maxDistance;
+	DogAction currentAction;
+	bool isWaiting;
+	public LeadPlayer(Dog d, Transform player, Transform target, float maxDistance) : base(d){
         this.player = player;
+		this.target = target;
+		this.maxDistance = maxDistance;
     }
+	public override void StartAction(){
+		currentAction = new FollowTarget (dog, target, true);
+		isWaiting = false;
+	}
+	//Måste även kolla om spelaren är närmare target etc..
     public override void UpdateAction(){
+		currentAction.UpdateAction ();
+		if (Vector3.Distance (player.position, dog.transform.position) < maxDistance || Vector3.Distance (player.position, target.position) < Vector3.Distance (dog.transform.position, target.position)) {
+			if (isWaiting) {
+				if (currentAction.IsDone ()) {
+					currentAction = new FollowTarget (dog, target, true);
+					isWaiting = false;
+				}
+			}
+		} else {
+			if (Vector3.Distance (player.position, target.position) > Vector3.Distance (dog.transform.position, target.position)) {
+				if (!isWaiting) {
+					currentAction.EndAction ();
+					currentAction = new WaitForPlayer (dog, player,target, maxDistance);
+					currentAction.StartAction ();
+					isWaiting = true;
+				}
+			}
+		}
     }
 }
