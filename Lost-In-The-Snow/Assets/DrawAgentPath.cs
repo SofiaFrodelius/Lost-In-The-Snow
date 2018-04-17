93371c71d22cc7d67e66d1e43ec696179894bf93
Sooolan <emil.rosenquist@gmail.com>
2018-04-17 19:16:25 +0200

    Locomotion dog

--- Lost-In-The-Snow/Assets/DrawAgentPath.cs
+++ Lost-In-The-Snow/Assets/DrawAgentPath.cs
@@ -11,27 +11,6 @@ public class DrawAgentPath : MonoBehaviour {
 	Vector2 velocity = Vector2.zero;
 	void Start () {
 		lr = GetComponent<LineRenderer> ();
-		navA.updatePosition = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 worldDeltaPosition = navA.nextPosition - navA.transform.position;
-
-		// Map 'worldDeltaPosition' to local space
-		float dx = Vector3.Dot (navA.transform.right, worldDeltaPosition);
-		float dy = Vector3.Dot (navA.transform.forward, worldDeltaPosition);
-		Vector2 deltaPosition = new Vector2 (dx, dy);
-
-		float smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
-		smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
-
-		// Update velocity if time advances
-		if (Time.deltaTime > 1e-5f)
-			velocity = smoothDeltaPosition / Time.deltaTime;
-		//print ("dx:" + dx + " dy:" + dy);
-		print ("Velx: " + velocity.x + " Vely: " + velocity.y);
-
-		lr.SetPositions (navA.path.corners);
+		//navA.updatePosition = false;
 	}
 }
