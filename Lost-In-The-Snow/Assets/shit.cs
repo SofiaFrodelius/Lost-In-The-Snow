59ab5323406e5608c98d44d6363d2576dd97b659
SofiaFrodelius <scris_love@hotmail.com>
2018-04-19 12:08:17 +0200

    Stuff to test saving inventory

--- Lost-In-The-Snow/Assets/shit.cs
+++ Lost-In-The-Snow/Assets/shit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 public class trigger : MonoBehaviour {
-    public coll trigger.collider();
+    //public coll trigger.collider();
 
     void OnTriggerEnter(Collider other)
     {
