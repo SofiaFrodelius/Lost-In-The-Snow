2f9fc68a22776fc60bf91d0e198a955b6c94beed
HolmErik <erikholm@live.com>
2018-04-19 10:15:41 +0200

    DayNightCycle +måne
    la till måne i daynight cycle och ändrade lite hur det fungerar.

--- Lost-In-The-Snow/Assets/DayNightCycle.cs
+++ Lost-In-The-Snow/Assets/DayNightCycle.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class DayNightCycle : MonoBehaviour
 {
     [SerializeField]
+    private Light sun,moon;
+
+    [SerializeField]
     private float timeUntilNight = 5.0f;
     private float targetBlend = 1.0f;
-
     [SerializeField]
     private AnimationCurve skyboxBlendCurve;
     [SerializeField]
     private AnimationCurve sunIntensityCurve;
     [SerializeField]
+    private AnimationCurve moonIntensityCurve;
+
+    [SerializeField]
     [Range(-10, 60)]
     private float sunTarget = 0.0f;
-    private float sunDelta = 0.0f;
-    private float currentSunMove = 0.0f;
+    private float rotationDelta = 0.0f;
+    private float currentMove = 0.0f;
 
 
     private float blend = 0;
     private float defaultBlend = 0;
     private float currentFrameTime = 0;
     private Animator anim;
+    private bool nightTime = false;
+    private float elapsedTime = 0.0f;
 
 
     [HideInInspector]
@@ -37,25 +44,53 @@ public class DayNightCycle : MonoBehaviour
         groundColor = RenderSettings.ambientGroundColor;
         anim = GetComponent<Animator>();
         anim.speed = 1 / timeUntilNight;
-        sunDelta = sunTarget - transform.GetChild(0).transform.localEulerAngles.x;
+        rotationDelta = sunTarget - sun.transform.localEulerAngles.x;
     }
 
 
     public void Update()
     {
-        if (anim.GetBool("Sundown") && RenderSettings.skybox.GetFloat("_Blend") < targetBlend)
+        if (anim.GetBool("Sundown") && RenderSettings.skybox.GetFloat("_Blend") < targetBlend/2)
         {
-            currentFrameTime = (1 / timeUntilNight) * Time.deltaTime; //1 because _Blend max value is 1 and min value is 0
-            currentSunMove = (sunDelta / timeUntilNight) * Time.deltaTime;
-
-            transform.GetChild(0).transform.Rotate(Vector3.right, currentSunMove); //rotate sun
-            blend += currentFrameTime;
+            updateBlend();
+            rotate(sun.gameObject);
             RenderSettings.sun.intensity = sunIntensityCurve.Evaluate(blend);
-            RenderSettings.skybox.SetFloat("_Blend", skyboxBlendCurve.Evaluate(blend));
-            RenderSettings.ambientSkyColor = skyColor;
-            RenderSettings.ambientEquatorColor = equatorColor;
-            RenderSettings.ambientGroundColor = groundColor;
+            setColors();
+            nightTime = true;
+            elapsedTime += Time.deltaTime;
+            Debug.Log(elapsedTime);
         }
+
+        else if(nightTime && RenderSettings.skybox.GetFloat("_Blend") < targetBlend)
+        {
+            updateBlend();
+            rotate(moon.gameObject);
+            moon.intensity = moonIntensityCurve.Evaluate(blend);
+            setColors();
+
+        }
+    }
+
+
+    private void rotate(GameObject go)
+    {
+        currentMove = (rotationDelta / (timeUntilNight)) * Time.deltaTime;
+        currentMove *= 2;
+        go.transform.Rotate(Vector3.right, currentMove);
+    }
+
+    private void updateBlend()
+    {
+        currentFrameTime = (1 / (timeUntilNight)) * Time.deltaTime; //1 because _Blend max value is 1 and min value is 0
+        blend += currentFrameTime;
+        RenderSettings.skybox.SetFloat("_Blend", skyboxBlendCurve.Evaluate(blend));
+    }
+
+    private void setColors()
+    {
+        RenderSettings.ambientSkyColor = skyColor;
+        RenderSettings.ambientEquatorColor = equatorColor;
+        RenderSettings.ambientGroundColor = groundColor;
     }
 
 
