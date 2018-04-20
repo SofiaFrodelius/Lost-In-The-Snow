7c7898b4c8cc0783b93f8140f66ac096183cc9b4
HolmErik <erikholm@live.com>
2018-04-20 09:19:20 +0200

    Revert till meta filer

---
+++ Lost-In-The-Snow/Assets/DoorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour, IInteractible
+{
+    [SerializeField]
+    private SceneSwitchScript swc;
+    public void Interact()
+    {
+        swc.ActivateSceneSwitch(1);
+    }
+}
