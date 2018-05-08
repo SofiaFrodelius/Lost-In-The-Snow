60db5b65a62684e5e029e0ad30be5d740a1ca75a
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-08 16:06:35 +0200

    Pushar animStates

--- Lost-In-The-Snow/Assets/Dog/Scripts/DogAI.cs
+++ Lost-In-The-Snow/Assets/Dog/Scripts/DogAI.cs
@@ -33,8 +33,8 @@ public class DogAI : MonoBehaviour {
 		bestMood.ChangeMood (100f, 50f, 75f, 0f);
 
 		//dog.currentAction = new RingARound (dog, dog.player, 5f);
-		//dog.currentAction = new Sit(dog, 5f);
-		//dog.currentAction.StartAction ();
+		dog.currentAction = new Sit(dog, 10f);
+		dog.currentAction.StartAction ();
 		//StartAction (ActionType.ACTIVE);
 
 	}
