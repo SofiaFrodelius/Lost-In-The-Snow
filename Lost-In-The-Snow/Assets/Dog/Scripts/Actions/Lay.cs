caa18e927b4adf7d1ee4f6d84e8178a439ea4cfc
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-09 15:57:05 +0200

    Hind sitta stå vila
    Fungerar ej 100

--- Lost-In-The-Snow/Assets/Dog/Scripts/Actions/Lay.cs
+++ Lost-In-The-Snow/Assets/Dog/Scripts/Actions/Lay.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class Lay : DogAction
 {
+    DogTimer timer;
     public Lay(Dog d, float time) : base(d)
     {
-
+        timer = new DogTimer(time);
     }
     public override void StartAction()
     {
+        actionTimer = actionDelay;
+        timer.ResetTimer();
+        isDone = false;
+        animator.SetBool("Lay", true);
     }
     public override void UpdateAction()
     {
+        if (animator.GetBool("Lay") && animator.GetCurrentAnimatorStateInfo(0).IsName("LayIdle"))
+        {
+            animator.SetBool("Lay", false);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("LayIdle"))
+        {
+            if (!isDone)
+            {
+                if (!timer.IsDone())
+                {
+                    timer.AddTime(Time.deltaTime);
+                }
+                else
+                {
+                    isDone = true;
+                    animator.SetTrigger("StandUp");
+
+                }
+            }
+        }
     }
     public override void EndAction()
     {
+        animator.SetBool("Lay", false);
+        dog.AddEffectToMood(moodEffect);
     }
 
-    }
+}
---
+++ Lost-In-The-Snow/Assets/Dog/Scripts/Actions/Rest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rest : DogAction
+{
+    DogTimer timer;
+    public Rest(Dog d, float time) : base(d)
+    {
+        timer = new DogTimer(time);
+    }
+    public override void StartAction()
+    {
+        actionTimer = actionDelay;
+        timer.ResetTimer();
+        isDone = false;
+
+        animator.SetBool("Rest", true);
+    }
+    public override void UpdateAction()
+    {
+        if (animator.GetBool("Rest") && animator.GetCurrentAnimatorStateInfo(0).IsName("RestIdle"))
+        {
+            animator.SetBool("Rest", false);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("RestIdle"))
+        {
+            if (!isDone)
+            {
+                if (!timer.IsDone())
+                {
+                    timer.AddTime(Time.deltaTime);
+                }
+                else
+                {
+                    isDone = true;
+                    animator.SetTrigger("StandUp");
+                }
+            }
+        }
+    }
+    public override void EndAction()
+    {
+        animator.SetBool("Rest", false);
+        dog.AddEffectToMood(moodEffect);
+    }
+
+}
