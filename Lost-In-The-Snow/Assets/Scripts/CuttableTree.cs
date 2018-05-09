cd10585634374154c4ac82ceca768c8e80e58384
Rasmus <rasmus.lindberg.93@gmail.com>
2018-05-09 15:44:43 +0200

    Woodcutting cutscene start

--- Lost-In-The-Snow/Assets/Scripts/CuttableTree.cs
+++ Lost-In-The-Snow/Assets/Scripts/CuttableTree.cs
@@ -13,9 +13,21 @@ public class CuttableTree : MonoBehaviour, IInteractible
             charMove = playerObj.GetComponent<CharacterMovement>();
         if (charMove != null)
         {
-            float hAngle = 0;
+            float hAngle;
+            Vector2 deltaVector = new Vector2(transform.position.x - charMove.transform.position.x, transform.position.z - charMove.transform.position.z);
+            //hAngle = Vector2.Angle(new Vector2(transform.position.x, transform.position.z), new Vector2(charMove.transform.position.x, charMove.transform.position.z));
+            hAngle = Vector2.SignedAngle(Vector2.up, deltaVector.normalized);
+            Debug.Log(deltaVector);
+            while (hAngle < 0)
+                hAngle += 360;
+            while (hAngle >= 360)
+                hAngle -= 360;
             Vector2 targetLook = new Vector2(hAngle, 0f);
-            charMove.ForceMovement(charMove.transform.position, targetLook);
+            Vector3 cuttingposition = new Vector3(-deltaVector.x, 0, -deltaVector.y);
+            cuttingposition.Normalize();
+            float cuttingDistance = 2;
+            charMove.ForceMovement(transform.position + new Vector3(cuttingposition.x * cuttingDistance, charMove.transform.position.y - transform.position.y,
+                cuttingposition.z * cuttingDistance), targetLook);
         }
     }
 }
