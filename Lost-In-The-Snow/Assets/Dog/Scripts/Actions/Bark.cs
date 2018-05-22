ecc00c4e8fa4d371d6492e26b65dd0d3ee829fa0
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-05-22 15:29:52 +0200

    Bark Is Back

--- Lost-In-The-Snow/Assets/Dog/Scripts/Actions/Bark.cs
+++ Lost-In-The-Snow/Assets/Dog/Scripts/Actions/Bark.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Bark : DogAction
 {
     public Bark(Dog d) : base(d)
     {
+        
     }
     public override void StartAction()
     {
+        GameObject barkEmitter = dog.transform.Find("BarkSoundEmitter").gameObject;
         isDone = false;
         animator.SetTrigger("Bark");
+        if (barkEmitter)
+        {
+            barkEmitter.GetComponent<StudioEventEmitter>().Play();
+        }
+        else
+        {
+            Debug.LogWarning("Dog has no GameObject BarkSoundEmitter Child with a fmod_EventStudioEmitter with barkSound");
+        }
     }
     public override void UpdateAction()
     {
